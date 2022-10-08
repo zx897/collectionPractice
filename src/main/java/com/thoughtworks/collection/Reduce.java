655172c2a1f745a276c1d3ab6b0c1a86e112940d
Qian Zhang <897157658@qq.com>
2022-10-08 22:19:01 +0800

    feat ：the second time to implement the function.

--- src/main/java/com/thoughtworks/collection/Reduce.java
+++ src/main/java/com/thoughtworks/collection/Reduce.java
@@ -1,6 +1,5 @@
 package com.thoughtworks.collection;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.List;
 
@@ -13,42 +12,69 @@ public class Reduce {
     }
 
     public int getMaximum() {
-        throw new NotImplementedException();
+        int maxNumber = this.arrayList.stream().mapToInt(Integer::intValue).max().getAsInt();
+        return maxNumber;
     }
 
     public double getMinimum() {
-        throw new NotImplementedException();
+        int minNumber  = this.arrayList.stream().mapToInt(Integer::intValue).min().getAsInt();
+        return minNumber;
     }
 
     public double getAverage() {
-        throw new NotImplementedException();
+        double averageNumber = this.arrayList.stream().mapToInt((x) -> x).summaryStatistics().getAverage();
+        return averageNumber;
     }
 
-    public double getMedian() { throw new NotImplementedException(); }
+    public double getMedian() {
+        int length = this.arrayList.size();
+        if ((length & 1) == 1) {
+            return this.arrayList.get(((length + 1) / 2 - 1));
+        }
+        return (float) (this.arrayList.get(length / 2 - 1) + this.arrayList.get(length / 2)) / 2;
+    }
 
     public double getOrderedMedian() {
-        throw new NotImplementedException();
+
+        return this.getMedian();
     }
 
     public int getFirstEven() {
-        throw new NotImplementedException();
+        int firstEven = this.arrayList.stream().filter(x -> x%2 == 0).findFirst().get();
+        return firstEven;
     }
 
     public int getIndexOfFirstEven() {
-        throw new NotImplementedException();
+
+        for (int i = 0; i < this.arrayList.size() - 1; i++) {
+            if ((this.arrayList.get(i) & 1) == 0) {
+                return i;
+            }
+        }
+        return 0;
     }
 
     public boolean isEqual(List<Integer> arrayList) {
-        throw new NotImplementedException();
-    }
-
-
+        return this.arrayList.equals(arrayList);
+        }
 
     public int getLastOdd() {
-        throw new NotImplementedException();
+        int temp = this.arrayList.get(0);
+        for (Integer integer : this.arrayList) {
+            if ((integer & 1) == 1) {
+                temp = integer;
+            }
+        }
+        return temp;
     }
 
     public int getIndexOfLastOdd() {
-        throw new NotImplementedException();
+        int index = 0;
+        for (int i = 0; i < this.arrayList.size(); i++) {
+            if ((this.arrayList.get(i) & 1) == 1) {
+                index = i;
+            }
+        }
+        return index;
     }
 }
